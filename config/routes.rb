4dc38956180270602945361087a0140803e528ea
saru1439s7 <d.n.saru39@gmail.com>
2021-08-01 04:13:02 +0000

    課題４

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/' =>'homes#top', as:'root'
-  get 'home/about' => 'homes#about'
+  root :to => "homes#top"
+  get "home/about" => "homes#about"
 
-  resources :users,only: [:show,:edit,:update,:index,:update,:destroy]
-  resources :books
+  resources :users,only: [:show,:edit,:update,:index,:update,:destroy] do
+   resource :relationships, only:[:create,:destroy] 
+   get 'followings', to: 'relationships#followings', as: 'followings'
+   get 'followers', to: 'relationships#followers', as: 'followers'
+end
 
+  resources :books,only: [:show,:index,:create,:edit,:update,:destroy] do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
+   resources :followers, only: [:create, :destroy]
+  end
 end
